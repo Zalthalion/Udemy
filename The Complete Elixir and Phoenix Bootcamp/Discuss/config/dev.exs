6b3ced8f80a6e245dd839e4eb61d06040141547b
Zalthalion <freidenfeldsjuris@gmail.com>
2022-08-25 17:22:15 +0300

    Completed Section 7
    Created a new phoenix project and launched it

--- The Complete Elixir and Phoenix Bootcamp/Discuss/config/dev.exs
+++ The Complete Elixir and Phoenix Bootcamp/Discuss/config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :discuss, Discuss.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "admin",
   hostname: "localhost",
   database: "discuss_dev",
   stacktrace: true,
