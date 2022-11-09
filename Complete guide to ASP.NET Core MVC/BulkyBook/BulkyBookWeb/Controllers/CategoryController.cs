98a42c31f55a77afdad4561c5590a00574caeb65
Zalthalion <freidenfeldsjuris@gmail.com>
2022-11-09 03:31:19 +0200

    Finished section 2
    went through CRUD, should refactor. Missing just the reading part

--- Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,12 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            var _categoryList = _db.Categories.AsEnumerable();
+            return View(_categoryList);
+        }
+
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(x => x.Id == id);  
+
+            if(category is null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id is null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(x => x.Id == id);
+
+            if(category is null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBookWeb/Models/Category.cs
+++ Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -10,6 +11,8 @@ namespace BulkyBookWeb.Models
         [Required]
         public string? Name { get; set; }
 
+        [DisplayName ("Display order")]
+        [Range(1,100, ErrorMessage = "Display number should be within the range from 1 to 100")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
