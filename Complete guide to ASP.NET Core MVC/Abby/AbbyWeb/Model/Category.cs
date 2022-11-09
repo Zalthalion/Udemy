83e5cb1536e49592643a0dd999447072faf72e00
Zalthalion <freidenfeldsjuris@gmail.com>
2022-11-09 09:35:04 +0200

    Established connection with database
    code first approach

---
+++ Complete guide to ASP.NET Core MVC/Abby/AbbyWeb/Model/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbbyWeb.Model
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public int DisplayOrder { get; set; }
+    }
+}
