0887c33f62e65c546f00d5cde9ee5b024e84ddf7
Zalthalion <freidenfeldsjuris@gmail.com>
2022-11-12 21:58:12 +0200

    Amend

--- Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -13,5 +13,7 @@ namespace BulkyBook.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
 
         public DbSet<Cover> Covers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
     }
 }
