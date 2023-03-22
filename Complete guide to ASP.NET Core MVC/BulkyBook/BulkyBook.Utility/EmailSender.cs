202b675c8223e7c04d674cdc828c793efa889fa5
Zalthalion <freidenfeldsjuris@gmail.com>
2023-03-22 08:11:09 +0200

    Added email stuff
    I did not like the email implementation, so I did not test this

--- Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBook.Utility/EmailSender.cs
+++ Complete guide to ASP.NET Core MVC/BulkyBook/BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 
 namespace BulkyBook.Utility
 {
@@ -6,7 +8,25 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            // Not a fan of this dummy implementation tho
+            // Creates the email
+            var emailToSend = new MimeMessage();
+
+            emailToSend.From.Add(MailboxAddress.Parse("hello@freidenfelds.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            // Sends the email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+
+                // Yeah there is no way I am gonna bluntly just write my creditentials here :D
+                emailClient.Authenticate("mygmail", "mygmailpassword");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
